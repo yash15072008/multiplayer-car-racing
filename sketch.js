5e93417782f6425ebdbc93a1fad634871793de47
yash15072008 <75427466+yash15072008@users.noreply.github.com>
2021-10-19 21:17:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,59 +1,16 @@
-var ball,database,position;
+var game , player , form;
+var gameState , allPlayer , playerCount;
+var database , car1 , car2 , car3 , car4;
+var c1 , c2 , c3 , c4;
 
-function setup() {
-    database = firebase.database();
-    createCanvas(600,600);
-
-    ball = createSprite(300,300,50,50);
-    ball.shapeColor= "red";
-
-    var ballPosition = database.ref('ball/position');
-    ballPosition.on("value", readPosition, showError);
+function preload(){
 
 }
 
-function draw() {
-
-    background("lightblue");
-
-    if(keyDown("left_arrow")){
-
-       writePosition(-3,0);
-    }
-    if(keyDown("right_arrow")){
-
-        writePosition(3,0);
-    }
-    if(keyDown("down_arrow")){
-
-        writePosition(0,3); 
-    }
-    if(keyDown("up_arrow")){
-
-        writePosition(0,-3);
-    }
-
-    drawSprites();
-}
-
-function writePosition(x,y) {
-
-    database.ref('ball/position').set({
-
-        'x': position.x + x,
-        'y': position.y + y
-    })
-}
-
-function readPosition(data) {
-
-    position = data.val();
-    ball.x = position.x;
-    ball.y = position.y;
+function setup(){
 
 }
 
-function showError() {
+function draw(){
 
-    console.log("error");
 }
